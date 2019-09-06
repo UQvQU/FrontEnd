28082c243419eac7f288173ec737000e8870f3c7
THXXZB <2991369095@qq.com>
2019-09-06 19:01:28 +0800

    Vue_打包发布项目

---
+++ qt_learn/vue/vueDemo/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <p class="massage">{{massage}}</p>
+  </div>
+</template>
+
+<script>
+  export default { //配置对象(与vue一致)
+    data () { //组件中必须写成函数形式
+      return{
+        massage: 'Hello Vue Component'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .massage{
+    color: #abe;
+    font-size: 30px;
+  }
+</style>
---
+++ qt_learn/vue/vueDemo/src/main.js
@@ -0,0 +1,12 @@
+// 入口js
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
