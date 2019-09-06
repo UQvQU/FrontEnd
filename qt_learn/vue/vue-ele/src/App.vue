a0f9de3fd5f8377c8a5f1ce53783243c212d5e75
THXXZB <2991369095@qq.com>
2019-09-06 18:58:05 +0800

    Vue项目--饿了么APP

---
+++ qt_learn/vue/vue-ele/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <v-header :seller="seller"></v-header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import header from '@/components/header/header.vue'
+export default {
+  name: 'App',
+  data () {
+    return {
+      seller: {}
+    }
+  },
+  components: {
+    'v-header': header
+  },
+  created () {
+    this.$http.get('https://www.easy-mock.com/mock/5d2ed7b41418505a5562ae1e/movie/vue-eleme-seller')
+    .then(res => {
+      console.log(res);
+      if (res.data.errno == 0) {
+        this.seller = Object.assign({}, this.seller, res.data.data);
+      }
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
