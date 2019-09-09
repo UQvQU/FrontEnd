a2ec477f671709725b040750cafe0b54c7e6c94f
THXXZB <2991369095@qq.com>
2019-09-09 20:31:55 +0800

    vue-去哪儿-项目开发

---
+++ qt_learn/vue/qunar-app/src/pages/home/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="">
+    <home-header/>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import HomeHeader from '../../components/Home-Head'
+export default {
+  name: '',
+  data () {
+    return {}
+  },
+  components: {
+    HomeHeader
+  }
+}
+</script>
+
+<style scoped>
+</style>
