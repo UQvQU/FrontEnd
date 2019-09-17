548d0082692d687da46457775255f55530803111
THXXZB <2991369095@qq.com>
2019-09-17 16:59:39 +0800

    vue-vuex之初体验

---
+++ qt_learn/vue/vue-components-communicate/src/components/common/count.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h3>{{msg}}</h3>
+    <hr/>
+    <h3>{{$store.state.count}}</h3>
+    <button @click="$store.commit('add')">+</button>
+    <button @click="$store.commit('reduce')">-</button>
+  </div>
+
+</template>
+
+<script type="text/ecmascript-6">
+import store from '@/store/index.js'
+export default {
+  name: '',
+  data () {
+    return {
+      msg: 'hello Vuex'
+    }
+  },
+  store,
+  components: {}
+}
+</script>
+
+<style scoped >
+</style>
