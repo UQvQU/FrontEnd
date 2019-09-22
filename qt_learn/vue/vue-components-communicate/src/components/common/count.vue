c83e27efb12f5ff43b7025b1c782d6b912d17ac2
THXXZB <2991369095@qq.com>
2019-09-22 10:39:03 +0800

    vue-store

--- qt_learn/vue/vue-components-communicate/src/components/common/count.vue
+++ qt_learn/vue/vue-components-communicate/src/components/common/count.vue
@@ -2,15 +2,22 @@
   <div>
     <h3>{{msg}}</h3>
     <hr/>
-    <h3>{{$store.state.count}}</h3>
-    <button @click="$store.commit('add')">+</button>
+    <!-- <h3>{{$store.state.count}}</h3> -->
+    <h3>{{count}}</h3>
+    <button @click="add(10)">+</button>
     <button @click="$store.commit('reduce')">-</button>
+    <p>{{date}}</p>
+    <hr/>
+    <p>{{money}}</p>
+    <button @click="makeMoneyAction">挣钱</button>
   </div>
 
 </template>
 
 <script type="text/ecmascript-6">
 import store from '@/store/index.js'
+// mapState:取state中的数据，mapMutations:取mutation中的方法
+import { mapState, mapMutations, mapGetters, mapActions } from 'vuex'
 export default {
   name: '',
   data () {
@@ -19,7 +26,27 @@ export default {
     }
   },
   store,
-  components: {}
+  components: {},
+  // computed: mapState(['date']),
+  // computed: {
+  //   date () {
+  //     return this.$store.state.date
+  //   }
+  // }
+  // computed: mapState({
+  //   date: state => state.date
+  // }),
+  computed: {
+    ...mapState(['date', 'money']),
+    // count () {
+    //   return this.$store.getters.count
+    // }
+    ...mapGetters(['count'])
+  },
+  methods: {
+    ...mapMutations(['add']),
+    ...mapActions(['makeMoneyAction'])
+  }
 }
 </script>
 
--- qt_learn/vue/vue-components-communicate/src/store/index.js
+++ qt_learn/vue/vue-components-communicate/src/store/index.js
@@ -4,19 +4,48 @@ import Vue from 'vue'
 Vue.use(Vuex)
 
 const state = {
-  count: 1
+  count: 1,
+  date: '2019.09.20',
+  money: 100
 }
-
+// mutations 同步  actions 异步
 const mutations = {
-  add (state) {
-    state.count++
+  add (state, n) {
+    state.count += n
   },
   reduce (state) {
     state.count--
+  },
+  makeMoney (state) {
+    state.money += 10
+  }
+}
+// 异步
+const actions = {
+  // makeMoneyAction (context) {
+  //   context.commit('makeMoney')
+  // }
+  // makeMoneyAction ({commit}) {
+  //   commit('makeMoney')
+  // }
+  makeMoneyAction ({commit}) {
+    setTimeout(() => {
+      commit('makeMoney')
+    }, 2000)
+    console.log('挣钱')
+  }
+}
+
+const getters = {
+  count: function (state) {
+    // return (state.count += 100)
+    return state.count
   }
 }
 
 export default new Vuex.Store({
   state,
-  mutations
+  mutations,
+  getters,
+  actions
 })
