4f3be969cd1c8eecbb1d913dc217609116343fb2
THXXZB <2991369095@qq.com>
2019-11-08 19:13:41 +0800

    spider

--- qt_learn/node/smzdm-youhui-spider/index.js
+++ qt_learn/node/smzdm-youhui-spider/index.js
@@ -7,7 +7,7 @@ const cheerio = require('cheerio');
 // GET POST
 // http.createServer
 // 以get的请求方式访问相应的资源
-https.get('https://www.smzdm.com/youhui/', function(res){
+https.get('https://www.baidu.com/', function(res){
     res.setEncoding('utf-8');
     // console.log(res);
     let html = '';
@@ -19,7 +19,7 @@ https.get('https://www.smzdm.com/youhui/', function(res){
     // 当数据传输完成时
     res.on('end',function(chunk){
         // 输出所有HTML的源码
-        // console.log(html);
+        console.log(html);
         const $ = cheerio.load(html,{
             // 不解析entity     decode 解析  
             decodeEntities: false     //html entity--> eg: &nbsp;、&lt;(<)、&gt;(>)
@@ -28,17 +28,18 @@ https.get('https://www.smzdm.com/youhui/', function(res){
         let lists = [];
         // 获取所有商品的html结构, 并对每一个商品
         //只有一个参数代表每次都在整个document中寻找
-        $('.list.list_preferential').each(
+        $('#wrapper').each(
             function(){
             const _this = this;
             
             //$('.itemName a') --> document.querySelectoor('.itemName a')
             //$('.itemName a', _this) --> $('.list.list_preferential').querySelector('.itemName a')
-            let title = $('.itemName a', _this).text();
-            let price = $('.red', _this).text();
-            let img = $('.picLeft img', _this).attr('src');
-            lists.push(title, price, img);
-            saveImage('./img/', img);
+            let title = $('a.manv', _this).text();
+            // let price = $('.red', _this).text();
+            // let img = $('.picLeft img', _this).attr('src');
+            console.log('title:', title)
+            lists.push(title);
+            // saveImage('./img/', img);
         })
         saveLists('./data/lists.json', lists);
         
