e10549e2fa41b3e3e38b0cbc6d9f260bb6f0432d
THXXZB <2991369095@qq.com>
2019-11-13 08:29:06 +0800

    node spider

---
+++ qt_learn/node/smzdm-youhui-spider/spider.js
@@ -0,0 +1,28 @@
+const https = require('https')
+const fs = require('fs')
+// const cheerio require('cheerio')
+
+let url = 'https://nc.meituan.com/ptapi/recommends'
+
+https.get(url, function (res) {
+  let chunks = [],
+      size = 0
+  res.on('data', function (chunk) {
+    console.log(chunk)
+    chunks.push(chunk)
+    size += chunks.length
+  })
+
+  res.on('end', function () {
+    console.log('传输完毕')
+    // console.log(chunks, size)
+    let data = chunks.toString('utf-8')
+    console.log(data)
+    // let dataStr = JSON.stringify(data).trim()
+    // console.log(dataStr)
+    fs.writeFile('./data/lists.json', data, function(err) {
+        if(err) throw err;
+        console.log('写入成功');
+    });
+  })
+})
