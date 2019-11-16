6c07573109c942701678c617a2c6a1ab323a6291
THXXZB <2991369095@qq.com>
2019-11-16 14:30:28 +0800

    webpack -- build -- 静态资源处理，css文件提取

--- qt_learn/webpack/webpack-react-tree-shaking/src/App.js
+++ qt_learn/webpack/webpack-react-tree-shaking/src/App.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
 import { hot } from 'react-hot-loader';
+// import'./app.scss'
+import styleObj from './app.scss'
+console.log(styleObj)
 
 class App extends React.Component {
 	render () {
 		return (
 			<div>
-        123456
+				<h2>hello world</h2>
+				<p className={styleObj.item}>o(*￣︶￣*)o</p>
+				{/* http://localhost:8080/asserts/mate.jpg  -->  在devServer中设置contentBase */}
+				<img src="./asserts/image/mate.jpg"/>
+        1234567998
 			</div>
 		)
 	}
