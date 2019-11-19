8bb6540788ff40513e6ff414acc05f9dbcdc13c8
THXXZB <2991369095@qq.com>
2019-11-19 19:53:16 +0800

    webpack配置vuecli手脚架

---
+++ qt_learn/vue/my-app/my-app/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="id">
+    <img src="./assets/tt.jpg" alt="">
+    <router-link to="/login">登录</router-link>
+    <router-link to="/">首页</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+body {
+  height: 500px;
+  background-color: #abe;
+}
+</style>
---
+++ qt_learn/vue/my-app/my-app/webpack.dev.config.js
@@ -0,0 +1,65 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+module.exports = {
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node-modules/
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use:[
+          // MiniCssExtractPlugin.loader,
+          process.env.NODE_ENV !== 'production' ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.join(__dirname, 'src/index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'index.css',
+      allChunks: true
+    })
+  ],
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
+  },
+  devServer: {
+    contentBase: path.join(__dirname, './dist'),
+    port: '8080',
+    open: true,
+    hot: true
+  }
+}
