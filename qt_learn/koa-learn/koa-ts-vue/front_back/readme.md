fa2fdb03bfe93992899c52dc68c0cc7e7ba90962
THXXZB <2991369095@qq.com>
2019-12-18 23:29:41 +0800

    koa ts sequelize vue

---
+++ qt_learn/koa-learn/koa-ts-vue/front_back/readme.md
@@ -0,0 +1,57 @@
+# 初始化
+
+  ./node_modules/.bin/sequelize  查看sequelize命令
+  sequelize init (./node_modules/.bin/sequelize init)
+
+  初始化完成之后生成的目录
+  config：配置目录
+  migrations： 迁移文件（数据库表结构）
+  seeders：种子文件（生成测试数据）
+  models：模型文件
+
+## 创建数据库
+
+- ./node_modules/.bin/sequelize.cmd db:create
+
+## 构建数据库迁移结构
+
+- ./node_modules/.bin/sequelize.cmd migration:create --name user
+
+## 数据库结构
+
+- user: 用户表
+存放用户的基本信息（用户名，密码等）
+
+- user-profile: 用户的扩展信息（不常用信息）
+存放扩展信息（性别，生日，个人简介等）
+
+- login-log: 用户的登录日志
+存放用户的登录日志（登录时间，IP信息）
+
+- category: 美食分类表
+存放美食分类信息（热菜，凉菜等）
+
+- cookbook: 美食详情信息
+存放美食信息（标题，图片等）
+
+- step: 美食烹饪步骤
+存放烹饪美食的步骤，在每一个步骤中有一个字段与cookbook表的id进行关联
+
+- comment: 评论表
+存放用户对每个具体的cookbook中的美食的评论
+
+- favorite: 收藏表
+存放用户收藏的美食
+
+## 生成表
+
+./node_modules/.bin/sequelize.cmd db:migrate 生成
+./node_modules/.bin/sequelize.cmd db:migrate:undo:table-name/all  删除表/所有表
+
+## 创建种子文件
+
+- seeders 文件下创建假数据
+./node_modules/.bin/sequelize.cmd seed:generate --name  user
+
+- 导入数据
+./node_modules/.bin/sequelize.cmd db:seed:all
---
+++ qt_learn/koa-learn/koa-ts-vue/front_back/seeders/20191218125052-user.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const md5 = require('md5')
+const moment = require('moment')
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('user', [
+      {
+        id: 1,
+        username: 'tt',
+        password: md5('123'),
+        disabled: false,
+        mobile: '11111111101',
+        email: '2991369095@qq.com',
+        created_ip_at: '127.0.0.1',
+        updated_ip_at: '127.0.0.1',
+        created_at: moment().format('YYYY-MM-DD HH:mm:ss'),
+        updated_at: moment().format('YYYY-MM-DD HH:mm:ss')
+      },
+      {
+        id: 2,
+        username: 'thx',
+        password: md5('123'),
+        disabled: false,
+        mobile: '11111111102',
+        email: '2991369096@qq.com',
+        created_ip_at: '127.0.0.2',
+        updated_ip_at: '127.0.0.2',
+        created_at: moment().format('YYYY-MM-DD HH:mm:ss'),
+        updated_at: moment().format('YYYY-MM-DD HH:mm:ss')
+      }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
