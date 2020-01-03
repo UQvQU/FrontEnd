d96b93bc3c01bb6fc545753e628ee61c53cef8bd
THXXZB <2991369095@qq.com>
2020-01-03 23:15:15 +0800

    爬楼梯

--- qt_learn/leetcode/dynamic-programming/code.js
+++ qt_learn/leetcode/dynamic-programming/code.js
@@ -1,17 +1,34 @@
-const map = new Map()
-var climbStairs = function(n) {
-  if (n < 0) return 0
+// const map = new Map()
+// var climbStairs = function(n) {
+//   if (n < 0) return 0
+//   if (n === 1) return 1
+//   if (n === 2) return 2
+
+//   if (map.has(n)) {
+//     return map.get(n)
+//   }
+
+//   const value = climbStairs(n - 1) + climbStairs(n - 2)
+//   map.set(n, value)
+//   return value
+
+// };
+
+function climbStairs(n) {
+  if (n <= 0) return 0
   if (n === 1) return 1
   if (n === 2) return 2
+  let a = 1
+  let b = 2
+  let temp
 
-  if (map.has(n)) {
-    return map.get(n)
+  for (let i = 3; i <= n; i++) {
+    temp = a + b
+    a = b
+    b= temp
   }
 
-  const value = climbStairs(n - 1) + climbStairs(n - 2)
-  map.set(n, value)
-  return value
-
-};
+  return temp
+}
 
 console.log(climbStairs(4))
