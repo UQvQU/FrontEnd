eb096ae3f06439bdd21cdff6abe01fcc93ee034e
THXXZB <2991369095@qq.com>
2020-01-08 23:27:11 +0800

    taro-ui react 小程序

---
+++ taro/readme.md
@@ -0,0 +1,12 @@
+# taro
+
+react + ts + hooks + sass + taro UI    小程序
+
+1. taro init demo
+创建项目
+2. npm run dev:weapp
+生成dist文件夹
+小程序开发工具包导入dist文件夹，查看效果
+3. npm i taro-ui
+使用taro UI
+config/index.js 配置 h5:{ esnextModules: ['taro-ui'] }
---
+++ taro/src/components/PostForm/index.jsx
@@ -0,0 +1,19 @@
+import Taro from '@tarojs/taro'
+import { View, Form, Input, Textarea } from '@tarojs/components'
+import { AtButton } from 'taro-ui'
+import './index.scss'
+
+export default function PostForm (props) {
+  return (
+    <View className='post-form'>
+      <View>新的帖子</View>
+      <Form onSubmit={props.handleSubmit}>
+        <View className='form-hint'>标题</View>
+        <Input className='input-title' type='text' placeholder='点击输入标题' value={props.formTitle} onInput={props.handleTitleInput}></Input>
+        <View className='form-hint'>正文</View>
+        <Textarea className='input-content' placeholder='点击输入正文' value={props.formContent} onInput={props.handleContentInput}></Textarea>
+        <AtButton formType='submit' type='primary'>发帖</AtButton>
+      </Form>
+    </View>
+  )
+}
